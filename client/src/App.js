c36e962ad56b054e6ae7d883f61fccc514890a35
syedomer17 <syedomerali2006@gmail.com>
2025-01-27 20:39:06 +0530

    placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,8 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import Pagination from "react-bootstrap/Pagination";
+import Placeholder from "react-bootstrap/Placeholder";
 
 function App() {
   const [show, setShow] = useState(false);
@@ -230,6 +232,35 @@ function App() {
             have chosen. Like, text, images, lists, etc.
           </Offcanvas.Body>
         </Offcanvas>
+      </div>
+      <br></br>
+      <div className="d-flex justify-content-around">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Placeholder as={Card.Title} animation="glow">
+              <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Text} animation="glow">
+              <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
+              <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
+              <Placeholder xs={8} />
+            </Placeholder>
+            <Placeholder.Button variant="primary" xs={6} />
+          </Card.Body>
+        </Card>
       </div>
     </>
   );
