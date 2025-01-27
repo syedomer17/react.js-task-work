c00ec1dd390a7d7b6a302687207219eb4ccc423a
syedomer17 <syedomerali2006@gmail.com>
2025-01-27 21:20:26 +0530

    React.js-BootStrap

--- client/src/App.js
+++ client/src/App.js
@@ -28,12 +28,25 @@ import Table from "react-bootstrap/Table";
 import Stack from "react-bootstrap/Stack";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 function App() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);//<ProgressBar now={60} />;
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
    return (
      <>
        <div>
@@ -457,6 +470,106 @@ function App() {
            <InputGroup.Text>With textarea</InputGroup.Text>
            <Form.Control as="textarea" aria-label="With textarea" />
          </InputGroup>
+       </div>
+       <br></br>
+       <div>
+         <FloatingLabel
+           controlId="floatingInput"
+           label="Email address"
+           className="mb-3"
+         >
+           <Form.Control type="email" placeholder="name@example.com" />
+         </FloatingLabel>
+         <FloatingLabel controlId="floatingPassword" label="Password">
+           <Form.Control type="password" placeholder="Password" />
+         </FloatingLabel>
+       </div>
+       <br></br>
+       <div>
+         <Form>
+           <Form.Group className="mb-3" controlId="formGroupEmail">
+             <Form.Label>Email address</Form.Label>
+             <Form.Control type="email" placeholder="Enter email" />
+           </Form.Group>
+           <Form.Group className="mb-3" controlId="formGroupPassword">
+             <Form.Label>Password</Form.Label>
+             <Form.Control type="password" placeholder="Password" />
+           </Form.Group>
+         </Form>
+       </div>
+       <br></br>
+       <div>
+         <Form noValidate validated={validated} onSubmit={handleSubmit}>
+           <Row className="mb-3">
+             <Form.Group as={Col} md="4" controlId="validationCustom01">
+               <Form.Label>First name</Form.Label>
+               <Form.Control
+                 required
+                 type="text"
+                 placeholder="First name"
+                 defaultValue="Mark"
+               />
+               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+             </Form.Group>
+             <Form.Group as={Col} md="4" controlId="validationCustom02">
+               <Form.Label>Last name</Form.Label>
+               <Form.Control
+                 required
+                 type="text"
+                 placeholder="Last name"
+                 defaultValue="Otto"
+               />
+               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+             </Form.Group>
+             <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+               <Form.Label>Username</Form.Label>
+               <InputGroup hasValidation>
+                 <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                 <Form.Control
+                   type="text"
+                   placeholder="Username"
+                   aria-describedby="inputGroupPrepend"
+                   required
+                 />
+                 <Form.Control.Feedback type="invalid">
+                   Please choose a username.
+                 </Form.Control.Feedback>
+               </InputGroup>
+             </Form.Group>
+           </Row>
+           <Row className="mb-3">
+             <Form.Group as={Col} md="6" controlId="validationCustom03">
+               <Form.Label>City</Form.Label>
+               <Form.Control type="text" placeholder="City" required />
+               <Form.Control.Feedback type="invalid">
+                 Please provide a valid city.
+               </Form.Control.Feedback>
+             </Form.Group>
+             <Form.Group as={Col} md="3" controlId="validationCustom04">
+               <Form.Label>State</Form.Label>
+               <Form.Control type="text" placeholder="State" required />
+               <Form.Control.Feedback type="invalid">
+                 Please provide a valid state.
+               </Form.Control.Feedback>
+             </Form.Group>
+             <Form.Group as={Col} md="3" controlId="validationCustom05">
+               <Form.Label>Zip</Form.Label>
+               <Form.Control type="text" placeholder="Zip" required />
+               <Form.Control.Feedback type="invalid">
+                 Please provide a valid zip.
+               </Form.Control.Feedback>
+             </Form.Group>
+           </Row>
+           <Form.Group className="mb-3">
+             <Form.Check
+               required
+               label="Agree to terms and conditions"
+               feedback="You must agree before submitting."
+               feedbackType="invalid"
+             />
+           </Form.Group>
+           <Button type="submit">Submit form</Button>
+         </Form>
        </div>
      </>
    );
