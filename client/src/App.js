5bef6c2467571a9284066c558dddca9e74480702
syedomer17 <syedomerali2006@gmail.com>
2025-01-27 20:17:19 +0530

    card

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Accordion from "react-bootstrap/Accordion";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
@@ -64,6 +66,20 @@ function App() {
         <Button variant="outline-info">Info</Button>
         <Button variant="outline-light">Light</Button>
         <Button variant="outline-dark">Dark</Button>
+      </div>
+      <br></br>
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
     </>
   );
