6ac89631c28efd9b54f052cb931acc14dcaec275
syedomer17 <syedomerali2006@gmail.com>
2025-01-27 20:57:17 +0530

    spinners

--- client/src/App.js
+++ client/src/App.js
@@ -22,89 +22,91 @@ import { useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Pagination from "react-bootstrap/Pagination";
 import Placeholder from "react-bootstrap/Placeholder";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Spinner from "react-bootstrap/Spinner";
 
 function App() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  return (
-    <>
-      <div>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      </div>
-      <br></br>
-      <div>
-        {[
-          "primary",
-          "secondary",
-          "success",
-          "danger",
-          "warning",
-          "info",
-          "light",
-          "dark",
-        ].map((variant) => (
-          <Alert key={variant} variant={variant}>
-            This is a {variant} alert with{" "}
-            <Alert.Link href="#">an example link</Alert.Link>. Give it a click
-            if you like.
-          </Alert>
-        ))}
-      </div>
-      <br></br>
-      <div>
-        <Button variant="outline-primary">Primary</Button>
-        <Button variant="outline-secondary">Secondary</Button>
-        <Button variant="outline-success">Success</Button>
-        <Button variant="outline-warning">Warning</Button>
-        <Button variant="outline-danger">Danger</Button>
-        <Button variant="outline-info">Info</Button>
-        <Button variant="outline-light">Light</Button>
-        <Button variant="outline-dark">Dark</Button>
-      </div>
-      <br></br>
-      <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
-      <br></br>
-      <div>
-        {/* <Carousel>
+  const handleShow = () => setShow(true);//<ProgressBar now={60} />;
+   return (
+     <>
+       <div>
+         <Accordion defaultActiveKey="0">
+           <Accordion.Item eventKey="0">
+             <Accordion.Header>Accordion Item #1</Accordion.Header>
+             <Accordion.Body>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+               reprehenderit in voluptate velit esse cillum dolore eu fugiat
+               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+               sunt in culpa qui officia deserunt mollit anim id est laborum.
+             </Accordion.Body>
+           </Accordion.Item>
+           <Accordion.Item eventKey="1">
+             <Accordion.Header>Accordion Item #2</Accordion.Header>
+             <Accordion.Body>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+               reprehenderit in voluptate velit esse cillum dolore eu fugiat
+               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+               sunt in culpa qui officia deserunt mollit anim id est laborum.
+             </Accordion.Body>
+           </Accordion.Item>
+         </Accordion>
+       </div>
+       <br></br>
+       <div>
+         {[
+           "primary",
+           "secondary",
+           "success",
+           "danger",
+           "warning",
+           "info",
+           "light",
+           "dark",
+         ].map((variant) => (
+           <Alert key={variant} variant={variant}>
+             This is a {variant} alert with{" "}
+             <Alert.Link href="#">an example link</Alert.Link>. Give it a click
+             if you like.
+           </Alert>
+         ))}
+       </div>
+       <br></br>
+       <div>
+         <Button variant="outline-primary">Primary</Button>
+         <Button variant="outline-secondary">Secondary</Button>
+         <Button variant="outline-success">Success</Button>
+         <Button variant="outline-warning">Warning</Button>
+         <Button variant="outline-danger">Danger</Button>
+         <Button variant="outline-info">Info</Button>
+         <Button variant="outline-light">Light</Button>
+         <Button variant="outline-dark">Dark</Button>
+       </div>
+       <br></br>
+       <div>
+         <Card style={{ width: "18rem" }}>
+           <Card.Img variant="top" src="holder.js/100px180" />
+           <Card.Body>
+             <Card.Title>Card Title</Card.Title>
+             <Card.Text>
+               Some quick example text to build on the card title and make up
+               the bulk of the card's content.
+             </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+           </Card.Body>
+         </Card>
+       </div>
+       <br></br>
+       <div>
+         {/* <Carousel>
           <Carousel.Item>
             <ExampleCarouselImage text="First slide" />
             <Carousel.Caption>
@@ -129,50 +131,50 @@ function App() {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel> */}
-      </div>
-      <br></br>
-      <div>
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Dropdown Button
-          </Dropdown.Toggle>
+       </div>
+       <br></br>
+       <div>
+         <Dropdown>
+           <Dropdown.Toggle variant="success" id="dropdown-basic">
+             Dropdown Button
+           </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-      <br></br>
-      <div>
-        <Container>
-          <Row>
-            <Col xs={6} md={4}>
-              <Image src="holder.js/171x180" rounded />
-            </Col>
-            <Col xs={6} md={4}>
-              <Image src="holder.js/171x180" roundedCircle />
-            </Col>
-            <Col xs={6} md={4}>
-              <Image src="holder.js/171x180" thumbnail />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <br></br>
-      <div>
-        <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
-      </div>
-      <br></br>
-      <div>
-        {/* { className="modal show" style={{ display: "block", position: "initial" }}
+           <Dropdown.Menu>
+             <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+           </Dropdown.Menu>
+         </Dropdown>
+       </div>
+       <br></br>
+       <div>
+         <Container>
+           <Row>
+             <Col xs={6} md={4}>
+               <Image src="holder.js/171x180" rounded />
+             </Col>
+             <Col xs={6} md={4}>
+               <Image src="holder.js/171x180" roundedCircle />
+             </Col>
+             <Col xs={6} md={4}>
+               <Image src="holder.js/171x180" thumbnail />
+             </Col>
+           </Row>
+         </Container>
+       </div>
+       <br></br>
+       <div>
+         <ListGroup>
+           <ListGroup.Item>Cras justo odio</ListGroup.Item>
+           <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+           <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+           <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+           <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+         </ListGroup>
+       </div>
+       <br></br>
+       <div>
+         {/* { className="modal show" style={{ display: "block", position: "initial" }}
         >
         <Modal.Dialog>
           <Modal.Header closeButton>
@@ -188,82 +190,91 @@ function App() {
             <Button variant="primary">Save changes</Button>
           </Modal.Footer>
         </Modal.Dialog> } */}
-      </div>
-      <br></br>
-      <div>
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
-      <br></br>
-      <div>
-        <Button variant="primary" onClick={handleShow}>
-          Launch
-        </Button>
+       </div>
+       <br></br>
+       <div>
+         <Navbar expand="lg" className="bg-body-tertiary">
+           <Container>
+             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+             <Navbar.Collapse id="basic-navbar-nav">
+               <Nav className="me-auto">
+                 <Nav.Link href="#home">Home</Nav.Link>
+                 <Nav.Link href="#link">Link</Nav.Link>
+                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                   <NavDropdown.Item href="#action/3.1">
+                     Action
+                   </NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.2">
+                     Another action
+                   </NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.3">
+                     Something
+                   </NavDropdown.Item>
+                   <NavDropdown.Divider />
+                   <NavDropdown.Item href="#action/3.4">
+                     Separated link
+                   </NavDropdown.Item>
+                 </NavDropdown>
+               </Nav>
+             </Navbar.Collapse>
+           </Container>
+         </Navbar>
+       </div>
+       <br></br>
+       <div>
+         <Button variant="primary" onClick={handleShow}>
+           Launch
+         </Button>
 
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            Some text as placeholder. In real life you can have the elements you
-            have chosen. Like, text, images, lists, etc.
-          </Offcanvas.Body>
-        </Offcanvas>
-      </div>
-      <br></br>
-      <div className="d-flex justify-content-around">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+         <Offcanvas show={show} onHide={handleClose}>
+           <Offcanvas.Header closeButton>
+             <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+           </Offcanvas.Header>
+           <Offcanvas.Body>
+             Some text as placeholder. In real life you can have the elements
+             you have chosen. Like, text, images, lists, etc.
+           </Offcanvas.Body>
+         </Offcanvas>
+       </div>
+       <br></br>
+       <div className="d-flex justify-content-around">
+         <Card style={{ width: "18rem" }}>
+           <Card.Img variant="top" src="holder.js/100px180" />
+           <Card.Body>
+             <Card.Title>Card Title</Card.Title>
+             <Card.Text>
+               Some quick example text to build on the card title and make up
+               the bulk of the card's content.
+             </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+           </Card.Body>
+         </Card>
 
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Placeholder as={Card.Title} animation="glow">
-              <Placeholder xs={6} />
-            </Placeholder>
-            <Placeholder as={Card.Text} animation="glow">
-              <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-              <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
-              <Placeholder xs={8} />
-            </Placeholder>
-            <Placeholder.Button variant="primary" xs={6} />
-          </Card.Body>
-        </Card>
-      </div>
-    </>
-  );
+         <Card style={{ width: "18rem" }}>
+           <Card.Img variant="top" src="holder.js/100px180" />
+           <Card.Body>
+             <Placeholder as={Card.Title} animation="glow">
+               <Placeholder xs={6} />
+             </Placeholder>
+             <Placeholder as={Card.Text} animation="glow">
+               <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
+               <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
+               <Placeholder xs={8} />
+             </Placeholder>
+             <Placeholder.Button variant="primary" xs={6} />
+           </Card.Body>
+         </Card>
+       </div>
+       <br></br>
+       <div>
+         <Spinner animation="border" role="status">
+           <span className="visually-hidden">Loading...</span>
+         </Spinner>
+       </div>
+     </>
+   );
 }
 
-export default App
+ export default App 
+ //BasicExample;
