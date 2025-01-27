4843abdd64c9dcc472c25d3ebb77c28b17dc030c
syedomer17 <syedomerali2006@gmail.com>
2025-01-27 20:58:47 +0530

    tables

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import Pagination from "react-bootstrap/Pagination";
 import Placeholder from "react-bootstrap/Placeholder";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import Spinner from "react-bootstrap/Spinner";
+import Table from "react-bootstrap/Table";
 
 function App() {
   const [show, setShow] = useState(false);
@@ -271,6 +272,38 @@ function App() {
          <Spinner animation="border" role="status">
            <span className="visually-hidden">Loading...</span>
          </Spinner>
+       </div>
+       <br></br>
+       <div>
+         <Table striped bordered hover>
+           <thead>
+             <tr>
+               <th>#</th>
+               <th>First Name</th>
+               <th>Last Name</th>
+               <th>Username</th>
+             </tr>
+           </thead>
+           <tbody>
+             <tr>
+               <td>1</td>
+               <td>Mark</td>
+               <td>Otto</td>
+               <td>@mdo</td>
+             </tr>
+             <tr>
+               <td>2</td>
+               <td>Jacob</td>
+               <td>Thornton</td>
+               <td>@fat</td>
+             </tr>
+             <tr>
+               <td>3</td>
+               <td colSpan={2}>Larry the Bird</td>
+               <td>@twitter</td>
+             </tr>
+           </tbody>
+         </Table>
        </div>
      </>
    );
