ba7aba78bd275c03465326a5cd5335681bc176f1
syedomer17 <syedomerali2006@gmail.com>
2025-01-27 21:16:20 +0530

    input-group

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,7 @@ import Spinner from "react-bootstrap/Spinner";
 import Table from "react-bootstrap/Table";
 import Stack from "react-bootstrap/Stack";
 import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function App() {
   const [show, setShow] = useState(false);
@@ -396,6 +397,66 @@ function App() {
            <option value="2">Two</option>
            <option value="3">Three</option>
          </Form.Select>
+       </div>
+       <br></br>
+       <div>
+         <Form>
+           {["checkbox", "radio"].map((type) => (
+             <div key={`default-${type}`} className="mb-3">
+               <Form.Check // prettier-ignore
+                 type={type}
+                 id={`default-${type}`}
+                 label={`default ${type}`}
+               />
+
+               <Form.Check
+                 disabled
+                 type={type}
+                 label={`disabled ${type}`}
+                 id={`disabled-default-${type}`}
+               />
+             </div>
+           ))}
+         </Form>
+       </div>
+       <br></br>
+       <div>
+         <InputGroup className="mb-3">
+           <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+           <Form.Control
+             placeholder="Username"
+             aria-label="Username"
+             aria-describedby="basic-addon1"
+           />
+         </InputGroup>
+
+         <InputGroup className="mb-3">
+           <Form.Control
+             placeholder="Recipient's username"
+             aria-label="Recipient's username"
+             aria-describedby="basic-addon2"
+           />
+           <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+         </InputGroup>
+
+         <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+         <InputGroup className="mb-3">
+           <InputGroup.Text id="basic-addon3">
+             https://example.com/users/
+           </InputGroup.Text>
+           <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+         </InputGroup>
+
+         <InputGroup className="mb-3">
+           <InputGroup.Text>$</InputGroup.Text>
+           <Form.Control aria-label="Amount (to the nearest dollar)" />
+           <InputGroup.Text>.00</InputGroup.Text>
+         </InputGroup>
+
+         <InputGroup>
+           <InputGroup.Text>With textarea</InputGroup.Text>
+           <Form.Control as="textarea" aria-label="With textarea" />
+         </InputGroup>
        </div>
      </>
    );
